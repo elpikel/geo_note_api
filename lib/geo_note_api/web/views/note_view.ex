fcd47caf26091370323388f4c6a970d9e2d3451f
Arkadiusz Plichta <el.pikel@gmail.com>
2017-05-15 23:39:54 +0200

    json format

--- lib/geo_note_api/web/views/note_view.ex
+++ lib/geo_note_api/web/views/note_view.ex
@@ -3,11 +3,11 @@ defmodule GeoNoteApi.Web.NoteView do
   alias GeoNoteApi.Web.NoteView
 
   def render("index.json", %{notes: notes}) do
-    %{data: render_many(notes, NoteView, "note.json")}
+    render_many(notes, NoteView, "note.json")
   end
 
   def render("show.json", %{note: note}) do
-    %{data: render_one(note, NoteView, "note.json")}
+    render_one(note, NoteView, "note.json")
   end
 
   def render("note.json", %{note: note}) do
--- lib/geo_note_api/web/views/place_view.ex
+++ lib/geo_note_api/web/views/place_view.ex
@@ -3,11 +3,11 @@ defmodule GeoNoteApi.Web.PlaceView do
   alias GeoNoteApi.Web.PlaceView
 
   def render("index.json", %{places: places}) do
-    %{data: render_many(places, PlaceView, "place.json")}
+    render_many(places, PlaceView, "place.json")
   end
 
   def render("show.json", %{place: place}) do
-    %{data: render_one(place, PlaceView, "place.json")}
+    render_one(place, PlaceView, "place.json")
   end
 
   def render("place.json", %{place: place}) do
