9eec29bea57a038d665be79f2f2a9ca9d922dcc2
Arkadiusz Plichta <el.pikel@gmail.com>
2017-05-15 20:37:44 +0200

    heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,12 @@
 use Mix.Config
 
+# Configure your database
+config :geo_note_api, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+  
 # For production, we often load configuration from external
 # sources, such as your system environment. For this reason,
 # you won't find the :http configuration below, but set inside
@@ -15,8 +22,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :geo_note_api, GeoNoteApi.Web.Endpoint,
   on_init: {GeoNoteApi.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "young-thicket-35712", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +70,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
